5ab98527001a954948589ffa3b9d0f69a712ba30
Nick Saika <nicksaika@gmail.com>
2013-09-17 00:18:25 -0400

    Got it working!

--- contextio.go
+++ contextio.go
@@ -6,7 +6,7 @@ package contextio
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/mrjones/oauth"
+	"github.com/nesv/oauth"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -19,46 +19,68 @@ const (
 
 func New(key, secret string) *Client {
 	oc := oauth.NewConsumer(key, secret, oauth.ServiceProvider{})
-	return &Client{consumer: oc}
+	return &Client{consumer: oc, key: key, secret: secret}
 }
 
 type Client struct {
 	consumer *oauth.Consumer
+	key      string
+	secret   string
+}
+
+/*
+Enables debugging of the OAuth consumer when set to true. Providing a value of
+false disables debugging.
+*/
+func (c *Client) EnableDebugging(debug bool) {
+	c.consumer.Debug(debug)
 }
 
 func (c *Client) Do(method string, parts ...string) (resp *http.Response, err error) {
 	// Build the URL.
 	url := fmt.Sprintf("%s/%s/%s", API_URL, API_VERSION, strings.Join(parts, "/"))
 
+	// Some empty params
+	params := make(map[string]string)
+
+	// The access token
+	accessToken := &oauth.AccessToken{}
+
 	// Call the appropriate function on the consumer, depending on the provided
 	// value of `method`.
 	switch strings.ToUpper(method) {
 	case "GET":
-		resp, err = c.consumer.Get(url, nil, nil)
+		resp, err = c.consumer.Get(url, params, accessToken)
 
 	case "POST":
-		resp, err = c.consumer.Post(url, nil, nil)
+		resp, err = c.consumer.Post(url, params, accessToken)
 
 	case "PUT":
-		resp, err = c.consumer.Put(url, "", nil, nil)
+		resp, err = c.consumer.Put(url, "", params, accessToken)
 
 	case "DELETE":
-		resp, err = c.consumer.Delete(url, nil, nil)
+		resp, err = c.consumer.Delete(url, params, accessToken)
 	}
 
 	return
 }
 
-func (c *Client) Unmarshal(f func() (*http.Response, error), v interface{}) error {
-	resp, err := f()
+/*
+Unmarshals the body of the provided HTTP response, into the provided type.
+
+For example, if you called:
+
+	response, err := c.Do("GET", "accounts")
+
+...you would call *c.Unmarshal() like so:
+
+	var accounts []contextio.Account
+	err = c.Unmarshal(response, &accounts)
+*/
+func (c *Client) Unmarshal(response *http.Response, v interface{}) error {
+	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return err
 	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
 	return json.Unmarshal(body, v)
 }
