bd17e8e784362aeac604904674419b994fb5220c
Nick Saika <nicksaika@gmail.com>
2013-10-22 22:54:08 -0400

    tests: Updated the tests

--- contextio_test.go
+++ contextio_test.go
@@ -1,32 +1,58 @@
 package contextio
 
 import (
+	"log"
 	"os"
 	"testing"
 )
 
+var (
+	testAccount Account
+	testClient  *Client
+	testKey     string
+	testSecret  string
+)
+
+func init() {
+	testKey = os.Getenv("CONTEXTIOKEY")
+	if len(testKey) == 0 {
+		log.Fatal("CONTEXTIOKEY is not defined")
+	}
+
+	testSecret = os.Getenv("CONTEXTIOSECRET")
+	if len(testSecret) == 0 {
+		log.Fatal("CONTEXTIOSECRET is not defined")
+	}
+}
+
 func TestClient(t *testing.T) {
-	key := os.Getenv("CONTEXTIOKEY")
-	if len(key) == 0 {
-		t.Fatal("CONTEXTIOKEY is not defined")
-	}
-
-	secret := os.Getenv("CONTEXTIOSECRET")
-	if len(secret) == 0 {
-		t.Fatal("CONTEXTIOSECRET is not defined")
-	}
-
-	client := New(key, secret)
-	resp, err := client.Do("GET", nil, "accounts")
+	testClient = New(testKey, testSecret)
+	resp, err := testClient.Do("GET", nil, "accounts")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	var accounts []Account
-	if err = client.Unmarshal(resp, &accounts); err != nil {
+	if err = testClient.Unmarshal(resp, &accounts); err != nil {
 		t.Fatal(err)
 	}
 	for i, account := range accounts {
 		t.Log("Account:", i, account.Username)
 	}
+
+	testAccount = accounts[0]
+}
+
+func TestMessages(t *testing.T) {
+	resp, err := testClient.Do("GET", nil, "accounts", testAccount.Id, "messages")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var messages []Message
+	if err = testClient.Unmarshal(resp, &messages); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Retrieved", len(messages), "messages")
 }
